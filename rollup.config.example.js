d6e42918fc57e7f227678fdb3d1444e095cb20d8
James Ralston <jcralstoniii@gmail.com>
2018-07-28 13:17:44 -0400

    Updating the rollup example config to add the external lib

--- rollup.config.example.js
+++ rollup.config.example.js
@@ -1,4 +1,7 @@
 import typescript from 'rollup-plugin-typescript2';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs'
+
 
 export default {
   input: './src/config.ts',
@@ -13,7 +16,14 @@ export default {
   plugins: [
     typescript({
       tsconfig: "tsconfig.json"
+    }),
+    nodeResolve(),
+    commonjs({
+      namedExports:{
+        'node_modules/cloner/build/cloner.node.js':['shallow']
+      }
     })
+
   ],
   external: ['TileBoard']
 }
